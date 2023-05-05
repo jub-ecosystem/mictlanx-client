d796572f2f364c81214871a865d144c75afc66ef
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2023-05-04 23:07:58 -0600

    Advanced exmaples [refactor]

--- examples/v3/advance/basic.py
+++ examples/v3/advance/basic.py
@@ -2,9 +2,9 @@ import pandas as pd
 import os
 from mictlanx.v3.interfaces.payloads import PutPayload
 from mictlanx.v3.client import Client
-from mictlanx.v3.services.auth import Auth
+from mictlanx.v3.services.auth import Xolo
 from mictlanx.v3.services.proxy import Proxy
-from mictlanx.v3.interfaces.payloads import GenerateTokenPayload
+from mictlanx.v3.interfaces.payloads import AuthTokenPayload
 from mictlanx.v3.services.replica_manger import ReplicaManager
 from nanoid import generate as nanoid_
 from requests_toolbelt import MultipartEncoder
@@ -26,7 +26,7 @@ def simple_put(path:str):
         data = f.read()
         content_type = M.from_file(path,mime=True)
         metadata ={ "size":str(len(data)),"content_type":content_type}
-        payload = PutPayload(key =nanoid_(),bytes = data,metadata = metadata)
+        payload = PutPayload(key =nanoid_(),data = data,metadata = metadata)
         print(payload,metadata)
         x = c.put(payload)
         print("CLIENT_PUT_RESULT",x)
@@ -60,15 +60,15 @@ def put_from_trace():
                 "size":str(size),"child":child,"content_type":content_type
             }
             # print(metadata)
-            payload = PutPayload(key =key,bytes = data,metadata = metadata)
+            payload = PutPayload(key =key,data = data,metadata = metadata)
             put_res = c.put(payload)
             print("PAYLOAD[{}]".format(index),payload,put_res,metadata)
             # i+=1
             
 def put_from_trace_proxy():
-    auth_payload = GenerateTokenPayload(password="top_secret",expires_in = 7200)
-    auth_service = Auth(ip_addr = "localhost", port=10000, api_version=3)
-    auth = auth_service.generate_token(auth_payload)
+    auth_payload = AuthTokenPayload(secret="top_secret",expires_in = 7200)
+    auth_service = Xolo(ip_addr = "localhost", port=10000, api_version=3)
+    auth = auth_service.auth(auth_payload)
     print("AUTH_RESPONSE",auth)
     for (index,row) in list(xel_trace.iterrows()):
         chimistreta_path = row["CHIMISTRETA_PATH"]
@@ -131,7 +131,7 @@ def put_from_trace_proxy():
 if __name__ == "__main__":
     # put_from_trace_proxy()
     rm_service   = ReplicaManager(ip_addr = "localhost", port=20000, api_version=3)
-    auth_service = Auth(ip_addr = "localhost", port=10000, api_version=3)
+    auth_service = Xolo(ip_addr = "localhost", port=10000, api_version=3)
     proxy_service = Proxy(ip_addr = "localhost", port=8080, api_version=3)
     c            = Client(rm_service = rm_service, auth_service = auth_service,proxy = proxy_service,password = "t0p53cR3T#")
     with open("/source/01.pdf","rb") as f:
@@ -141,6 +141,6 @@ if __name__ == "__main__":
             "tag_2":"1000",
             "tag_3":J.dumps({"tag1":"VALUE","tag2":"2"})
         }
-        payload = PutPayload(key="ball-0",bytes = data,metadata=metadata)
+        payload = PutPayload(key="ball-0",data = data,metadata=metadata)
         res = c.put(payload)
         print(res)
