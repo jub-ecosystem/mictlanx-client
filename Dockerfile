68abb1b866669b5b15ce332b48dae6f47ed0d098
Leo Garcia <21690080@tecvalles.mx>
2025-10-31 00:40:36 -0600

    Update script run_docs.sh; add mictlanx-docs.yml

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM python:3.9-alpine
 WORKDIR /app
 RUN pip install mkdocs \
-    mkdocstrings[python] \ 
+    mkdocstrings[python] \
     mkdocs-material \
     pymdown-extensions 
 RUN pip install mkdocstrings
 
 COPY ./mictlanx /app/mictlanx
 COPY ./mkdocs.yml .
+COPY ./stylesheets /app/stylesheets
 COPY ./docs /app/docs
 
 EXPOSE 8000
--- run_docs.sh
+++ run_docs.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-docker rm -f mictlanx-docs || true
-docker run -d --name mictlanx-docs -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material
+IMAGE_NAME="mictlanx-docs"
+CONTAINER_NAME="mictlanx-mkdocs"
+
+if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Removing old container: $CONTAINER_NAME"
+    docker rm -f $CONTAINER_NAME
+fi
+
+echo "Building image: $IMAGE_NAME"
+docker build -t $IMAGE_NAME .
+
+echo "Starting container: $CONTAINER_NAME"
+docker run -d \
+  --name $CONTAINER_NAME \
+  -p 8000:8000 \
+  $IMAGE_NAME
+
+echo "Container $CONTAINER_NAME is running at http://localhost:8000"
+
